1c093f1ddc686b4c84ac4d4d2a1589340e5c0375
Nichafah <nichapat_koth@cmu.ac.th>
2025-11-29 16:48:22 +0700

    Task8-9

--- src/Task8.ts
+++ src/Task8.ts
@@ -1,7 +1,30 @@
-let x:number = 8
-let y:number = 16
-if (x > 5 &&  y>12) {
-    console.log('ค่ามากจัง')
-}else {
-    console.log(x*y + x/y)
+// let x:number = 8
+// let y:number = 16
+// if (x > 5 &&  y>12) {
+//     console.log('ค่ามากจัง')
+// }else {
+//     console.log(x*y + x/y)
+// }
+
+
+// let x =15
+// if (x > 10) {
+//     console.log('x is more than 10')
+// } else if (x < 5){
+//     console.log('x is less than 5')
+// }
+// else {
+//     console.log('x is between 5 and 10')
+// }
+
+
+
+let x =90
+if (x > 80) {
+    console.log('เกรด A')
+} else if (x > 60){
+    console.log('เกรด B')
+}
+else {
+    console.log('เกรด C')
 }
