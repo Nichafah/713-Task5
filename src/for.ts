094cf59a4a86980e9e2e9a3ab3afd04a1b51eb2a
Nichafah <nichapat_koth@cmu.ac.th>
2025-11-29 17:10:51 +0700

    for-name

---
+++ src/for.ts
@@ -0,0 +1,21 @@
+// let i = 0;
+// for (i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+// for (let i = 0; i < color.length; i++) {
+//     console.log(color[i])
+// }
+let name_list = ['Fah', 'Zea', 'Toon', 'Benz', 'W', 'Lightttttt']
+for (let i = 0; i <name_list.length; i++) {
+const name = name_list[i]
+    if(name && name.length > 6) {
+        console.log(`สวัสดีคุณ ${name}`)
+
+    }
+    else {
+        console.log(`Hello ${name}`)
+
+
+    }
+}
