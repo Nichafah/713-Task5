c13ee0d57e15eaf76ee700f34563b0fe39927f04
Nichafah <nichapat_koth@cmu.ac.th>
2025-11-29 17:13:45 +0700

    Task10

---
+++ src/Task10.ts
@@ -0,0 +1,8 @@
+// let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+// for (const c of color) {
+//     console.log(c);
+// }
+let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+for (const index in color) {
+    console.log(`Index: ${index}, Value: ${color[index]}`);
+}
